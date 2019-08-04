bc94489ec15e7578cc37dab04f5054ea66627b07
Pimpan Tantivaravong <pimpannt@gmail.com>
2019-08-04 18:00:27 +0700

    refact code anf introduce next())

--- myapp/app.js
+++ myapp/app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/', (req,res) => res.send('Got a POST request'))
-app.put('/', (req,res) => res.send('Got a PUT request'))
-app.delete('/', (req,res) => res.send('Got a DELETE request'))
+app.get('/',function(req,res){
+  res.send('hello word')
+})
+
+app.post('/',function(req,res){
+ res.send('hello POST')
+})
+
+app.all('/secret',function(req,res,next){
+ console.log('Access the secret section ...')
+ next()
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

bd2dffb55e304f3aba43f231b6ca844f4216a728
Pimpan Tantivaravong <pimpannt@gmail.com>
2019-08-04 18:35:43 +0700

    introduce 2 callbacks

--- myapp/app.js
+++ myapp/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//single callback
 app.get('/',function(req,res){
   res.send('hello word')
 })
@@ -15,4 +16,18 @@ app.all('/secret',function(req,res,next){
  next()
 })
 
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  res.send(req.params)
+})
+
+//multi callbacks
+app.get('/example/b',function (req,res,next){
+  console.log('the response will be sent by the next function ...')
+  next()
+}, function (req,res){
+  res.send('Hello from B!')
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+

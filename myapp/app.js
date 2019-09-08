3db467691245303ac75be33e08b255c49e81eafc
Pimpan Tantivaravong <pimpannt@gmail.com>
2019-09-08 16:30:38 +0700

    start project with nodemon introduction

--- myapp/app.js
+++ myapp/app.js
@@ -2,32 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//single callback
-app.get('/',function(req,res){
-  res.send('hello word')
+app.get('/test',(req,res) => {
+  res.send('Hello World')
 })
 
-app.post('/',function(req,res){
- res.send('hello POST')
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
 })
-
-app.all('/secret',function(req,res,next){
- console.log('Access the secret section ...')
- next()
-})
-
-app.get('/users/:userId/books/:bookId', function (req, res) {
-  res.send(req.params)
-})
-
-//multi callbacks
-app.get('/example/b',function (req,res,next){
-  console.log('the response will be sent by the next function ...')
-  next()
-}, function (req,res){
-  res.send('Hello from B!')
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
